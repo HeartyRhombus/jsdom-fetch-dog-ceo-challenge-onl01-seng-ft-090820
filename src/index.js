a2355ca46be4695b3685b613b13a1212818ba959
HeartyRhombus <kaylahmg@gmail.com>
2020-12-14 09:53:19 -0800

    commit tree ce82d16cc35362f8fad771d10f9b0130063afe7f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+
+function dogCeos(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch('imgUrl')
+    .then(resp => resp.json)
+    .then(json => console.log(json))
+}
+
+function renderImage(json) {
+
+}
